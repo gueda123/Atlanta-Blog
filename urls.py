64489a3055d0035e2241ca053ccab4cf66c23506
gueda123 <60408640+gueda123@users.noreply.github.com>
2022-08-12 12:03:49 -0400

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,33 +1,12 @@
-"""blog URL Configuration
+from django.urls import path
+from . import views
+from .views import blog, search, CategoryView, blogdetail
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from graphene_django.views import GraphQLView
-from blog.schema import schema
-from users import views as user_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
-    path('register/', user_views.register, name='register'),
-    path('login/', user_views.login, name='login'),
-    path('logout/', user_views.logout, name='logout'),
-    path('', include('blogApp.urls')),
+path('', blog.as_view(), name="blog"),
+path('search', search, name="search"),
+path('category/<str:cats>/', CategoryView, name="category"),
+path('<slug:slug>/send-comment', views.send_comment, name="send_comment"),
+path('<slug:slug>/', blogdetail.as_view(), name="blog-detail"),
 ]
-
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
